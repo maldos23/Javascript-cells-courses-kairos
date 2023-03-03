b1e4572cfd9ae0c86ff82beeb021866e6873c335
“danodev” <daniel.bj.rd@gmail.com>
2023-03-03 12:22:34 -0600

    tarea 1 fechas

--- src/model/cars.js
+++
@@ -1,9 +0,0 @@
-const carsModel = [
-  {
-    name: "Yaris",
-    brand: "Toyota",
-    year: "2023",
-  },
-];
-
-module.exports = carsModel;
--- src/routes/index.js
+++
@@ -1,8 +0,0 @@
-const carsRouter = require("./cars.js");
-const { Router } = require("express");
-
-const initCarsRouter = Router();
-
-initCarsRouter.use(carsRouter);
-
-module.exports = initCarsRouter;
